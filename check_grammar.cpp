40147df18887aff9ac734311aa26aa31f31a7e41
Abhinav1004 <abhinavkrjha10@gmail.com>
2018-02-20 22:41:49 +0530

    First commit

---
+++ check_grammar.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main() {
+	char string[50];
+	int flag,count=0;
+	printf("The grammar is: S->aS, S->Sb, S->ab\n");
+	printf("Enter the string to be checked:\n");
+	cin>>string;
+	if(string[0]=='a') {
+		flag=0;
+		for (count=1;string[count-1]!='\0';count++) {
+			if(string[count]=='b') {
+				flag=1;
+				continue;
+			} else if((flag==1)&&(string[count]=='a')) {
+				printf("The string does not belong to the specified grammar");
+				break;
+			} else if(string[count]=='a')
+			continue;
+			else if((flag==1)&&(string[count]='\0')) {
+				printf("String accepted…..!!!!");
+				break;
+			} else {
+				printf("String not accepted");
+			}
+		}
+	}
+	return 0;
+}
